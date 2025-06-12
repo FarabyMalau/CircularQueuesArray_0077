c0676a511879e9cd102f9d2e016af39e47533452
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-12 17:51:09 +0600

    memeriksa lokasi berada terakhir array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -36,6 +36,13 @@ public:
             REAR = 0; // 2.b
         }
         else
-        {}
+        {
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array 
+            if (REAR == max -1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 }
